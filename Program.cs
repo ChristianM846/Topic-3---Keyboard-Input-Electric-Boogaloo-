c3e7ac587c693f3651d8f576b583130dc46225aa
Christian Moyes <339463846@GoTVDSB.ca>
2024-09-09 08:14:51 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Topic_3___Keyboard_Input__Electric_Boogaloo_
+{
+    //Christian Moyes
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+
+
+
+
+
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
